6418c6a1ac837fcfd3f222b4a58fd7efc951bba3
TrekkieEnderman <kathyfact@gmail.com>
2025-04-03 14:40:38 -0400

    chore: update copyright year

--- core/src/main/java/io/github/TrekkieEnderman/advancedgift/AdvancedGift.java
+++ core/src/main/java/io/github/TrekkieEnderman/advancedgift/AdvancedGift.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 TrekkieEnderman
+ * Copyright (c) 2025 TrekkieEnderman
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
